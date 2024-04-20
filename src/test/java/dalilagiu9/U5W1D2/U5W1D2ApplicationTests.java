e853a6fe450cd10690476e0018c44576f1ced358
dalilagiu98 <148708941+dalilagiu98@users.noreply.github.com>
2024-04-20 21:55:08 +0200

    Initial commit

---
+++ src/test/java/dalilagiu9/U5W1D2/U5W1D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dalilagiu9.U5W1D2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W1D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
